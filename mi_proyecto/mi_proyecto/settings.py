a0135781a18ac623382c611fa301695ee41f52fa
Eduardo Sanchez <eduardo.sanchez41@inacapmail.cl>
2026-09-06 16:27:38 -0300

    aa
    aaa

--- mi_proyecto/mi_proyecto/settings.py
+++ mi_proyecto/mi_proyecto/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jmw9^b_dy8h-u8c3he$ct1u*if0m@p*ud)&-rb*pbbcl*b2tq='
+SECRET_KEY = 'django-insecure-dbt%jh+d0mv8fechp@+t_(+^md=3=fe9x=f_mao!6)+*p_35&!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bienvenida',
-    'inventario'
 ]
 
 MIDDLEWARE = [
